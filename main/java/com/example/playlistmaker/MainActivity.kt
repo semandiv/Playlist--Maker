333dd4d34c441d7381adb82fd072ebd10bc36cde
Andrei Semenov <sand-nlp@ya.ru>
2024-05-19 16:12:02 +0300

    Второе задание - Toast при нажатии кнопок на главном экране

--- main/java/com/example/playlistmaker/MainActivity.kt
+++ main/java/com/example/playlistmaker/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.example.playlistmaker
 
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -15,6 +18,28 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        val searchButton = findViewById<Button>(R.id.search_button)
+        val libraryButton = findViewById<Button>(R.id.library_button)
+        val settingsButton = findViewById<Button>(R.id.settings_button)
+
+        //первый способ - через анонимный класс
+        val searchButtonClickListener: View.OnClickListener = object: View.OnClickListener {
+            override fun onClick(v: View?) {
+                Toast.makeText(this@MainActivity, "Нажали кнопку Поиск", Toast.LENGTH_LONG).show()
+            }
+        }
+        searchButton.setOnClickListener(searchButtonClickListener)
+
+        //второй способ - через лямбду
+        libraryButton.setOnClickListener{
+            Toast.makeText(this@MainActivity, "Нажали кнопку Медиатека", Toast.LENGTH_LONG).show()
+        }
+
+        //тоже через лямбду
+        settingsButton.setOnClickListener{
+            Toast.makeText(this@MainActivity, "Нажали кнопку Настройки", Toast.LENGTH_LONG).show()
         }
     }
 }
