553603553fc5743f329f05477127b39d250e9d68
Andrei Semenov <sand-nlp@ya.ru>
2024-05-19 16:21:59 +0300

    Третье задание - навигация между экранами

--- main/java/com/example/playlistmaker/MainActivity.kt
+++ main/java/com/example/playlistmaker/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.playlistmaker
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
@@ -27,19 +28,22 @@ class MainActivity : AppCompatActivity() {
         //первый способ - через анонимный класс
         val searchButtonClickListener: View.OnClickListener = object: View.OnClickListener {
             override fun onClick(v: View?) {
-                Toast.makeText(this@MainActivity, "Нажали кнопку Поиск", Toast.LENGTH_LONG).show()
+                val searchIntent = Intent(this@MainActivity, SearchActivity::class.java)
+                startActivity(searchIntent)
             }
         }
         searchButton.setOnClickListener(searchButtonClickListener)
 
         //второй способ - через лямбду
         libraryButton.setOnClickListener{
-            Toast.makeText(this@MainActivity, "Нажали кнопку Медиатека", Toast.LENGTH_LONG).show()
+            val libraryIntent = Intent(this@MainActivity, LibraryActivity::class.java)
+            startActivity(libraryIntent)
         }
 
         //тоже через лямбду
         settingsButton.setOnClickListener{
-            Toast.makeText(this@MainActivity, "Нажали кнопку Настройки", Toast.LENGTH_LONG).show()
+            val settingsIntent = Intent(this@MainActivity, SettingsActivity::class.java)
+            startActivity(settingsIntent)
         }
     }
 }
--- main/java/com/example/playlistmaker/SettingsActivity.kt
+++ main/java/com/example/playlistmaker/SettingsActivity.kt
@@ -1,20 +1,29 @@
 package com.example.playlistmaker
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.ImageView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class activity_settings : AppCompatActivity() {
+class SettingsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_settings)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.settings)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        val backArrow = findViewById<ImageView>(R.id.back_arrow)
+
+        backArrow.setOnClickListener {
+            val mainIntent = Intent(this@SettingsActivity, MainActivity::class.java)
+            startActivity(mainIntent)
         }
     }
 }
